c6ad5413ca5ea055d83c7658df6c4a1cddb42879
Nathan Acks <nathan.acks@cardboard-iguana.com>
2025-12-10 07:07:28 -0700

    Notes and migration

--- flake.nix
+++ flake.nix
@@ -27,10 +27,13 @@
         f {
           pkgs = import nixpkgs {
             inherit system;
-            config.allowUnfree = true;
+            config.allowUnfree = true; # Many security tools have "unfree" licenses
           };
         });
   in {
+    # The forEachSystem wrapper function ensures that
+    # devShells.${system}.default always exists
+    #
     devShells = forEachSystem ({pkgs}: {
       default = pkgs.mkShell {
         packages = with pkgs; [
@@ -55,6 +58,16 @@
         # TODO: Allow Python packages to be added using requirements.txt (watch this! but is there a better format? maybe `uv`?)
         # TODO: Allow Node packages to be added using package.json (watch this! but is there a better format? maybe `pnpm`?)
         # TODO: Install `hardhat` via above (maybe https://gist.github.com/mccutchen/e9c0ba406d4b89147b97c2329d65d740)
+        # TODO: Can I alias `pnpm` to `npm`? Should I? (Maybe I should install `corepack`?)
+        # TODO: Replicate core packages from Disposable Kali
+        # TODO: Figure out how to move as many package configurations into the environment as possible
+        #         - Shodan
+        #         - Burp Suite
+        #         - ...
+        # TODO: Figure out if it's possible to start/stop Asciinema on direnv init/deinit
+        # TODO: Look into direnv for things like Metasploit Postgress DB (though it woul dbe better to use something like SQLite...)
+        # TODO: Create backup script (exclude .direnv, node_modules - can I just use my .gitignore?)
+        # TODO: Create init.sh script
       };
     });
   };
