1e2b9ad1077a32c64544d0b84860284dc86ef473
Nathan Acks <nathan.acks@cardboard-iguana.com>
2025-12-15 15:25:59 -0700

    wrapShell

--- scripts/shlib/libenv.sh
+++ scripts/shlib/libenv.sh
@@ -25,3 +25,47 @@ function unmask_executable {
 
     export PATH="$OLD_PATH"
 }
+
+function get_msf_config_root {
+    ENGAGEMENT_DIRECTORY="$(get_direnv_path "$1")"
+
+    if [[ -n "$ENGAGEMENT_DIRECTORY" ]] && [[ -d "$ENGAGEMENT_DIRECTORY" ]]; then
+        if [[ -z "$MSF_CFGROOT_CONFIG" ]]; then
+            MSF_CFGROOT_CONFIG="$ENGAGEMENT_DIRECTORY"/appdata/metasploit
+            mkdir -p "$MSF_CFGROOT_CONFIG"
+        fi
+        echo "$MSF_CFGROOT_CONFIG"
+    fi
+}
+
+function start_msfdb {
+    MSF_CFGROOT_CONFIG="$(get_msf_config_root "$1")"
+
+    if [[ -n "$MSF_CFGROOT_CONFIG" ]]; then
+        export MSF_CFGROOT_CONFIG
+
+        if [[ ! -d "$MSF_CFGROOT_CONFIG"/db ]]; then
+            msfdb init
+        fi
+        if [[ -f "$MSF_CFGROOT_CONFIG"/db/postmaster.pid ]] && [[ -z "$(pgrep -F "$MSF_CFGROOT_CONFIG"/db/postmaster.pid)" ]]; then
+            rm "$MSF_CFGROOT_CONFIG"/db/postmaster.pid
+        fi
+        if [[ ! -f "$MSF_CFGROOT_CONFIG"/db/postmaster.pid ]]; then
+            msfdb start
+        fi
+    fi
+}
+
+function stop_msfdb {
+    ENGAGEMENT_DIRECTORY="$(get_direnv_path "$1")"
+    MSF_CFGROOT_CONFIG="$(get_msf_config_root "$ENGAGEMENT_DIRECTORY")"
+
+    if [[ -n "$MSF_CFGROOT_CONFIG" ]]; then
+        export MSF_CFGROOT_CONFIG
+
+        if [[ $(pgrep -f "msfconsole msfConfigRoot=$MSF_CFGROOT_CONFIG" | wc -l) -eq 0 ]] &&
+            [[ $(pgrep -a -f "asciinema rec .* $ENGAGEMENT_DIRECTORY/artifacts/terminal_session_.*.cast" | wc -l) -eq 0 ]]; then
+            msfdb stop
+        fi
+    fi
+}
